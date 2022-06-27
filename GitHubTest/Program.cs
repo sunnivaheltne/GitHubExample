a17ab988942f4a9493e1f921d4b52e7c22aad139
Sunniva Botn Heltne <sunniva.heltne@gmail.com>
2022-06-27 10:00:20 +0200

    Added WriteLine that writes out your name.

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -7,6 +7,8 @@
             Console.WriteLine("Hello, Academy!");
             Console.WriteLine("What's your name?");
             var name = Console.ReadLine();
+
+            Console.WriteLine($"Your name is {name}");
         }
     }
 }
